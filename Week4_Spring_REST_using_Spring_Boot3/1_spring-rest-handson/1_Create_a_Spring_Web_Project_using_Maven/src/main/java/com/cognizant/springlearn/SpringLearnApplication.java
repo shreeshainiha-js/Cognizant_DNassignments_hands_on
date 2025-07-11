7817f6a4a7040de7623a18040a5e22ebfc4bfc5d
shreeshainiha-js <shreeshainiha27@gmail.com>
2025-07-11 20:43:17 +0530

    Added Spring Boot project files to Week 4 assignment

---
+++ Week4_Spring_REST_using_Spring_Boot3/1_spring-rest-handson/1_Create_a_Spring_Web_Project_using_Maven/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,23 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+@SpringBootApplication
+public class SpringLearnApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnApplication.class, args);
+        LOGGER.info("Application Started");
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Country country = (Country) context.getBean("country");
+        System.out.println(country);
+    
+    }
+}
+ 
